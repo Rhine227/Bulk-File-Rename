1b03e2adc825d781f83bcbd54b77253befcc5f60
Rhine227 <rhine227@gmail.com>
2022-09-22 06:38:27 -0500

    summary

---
+++ renamer/app.py
@@ -0,0 +1,4 @@
+import sys
+from PyQt5 import QApplication as qapp
+from .views import Window
+
---
+++ renamer/views.py
@@ -0,0 +1,15 @@
+# Provides the main window
+
+import sys
+from PyQt5 import QtWidgets
+from window import Ui_Window
+sys.path.insert(0, 'C:\\Users\\Rhine\\Documents\\GitHub\\Bulk-File-Rename\\ui')
+
+
+class Window(QtWidgets, Ui_Window):
+    def __init__(self):
+        super().__init__()
+        self.setupUI()
+
+    def _setupUI(self):
+        self._setupUI(self)
